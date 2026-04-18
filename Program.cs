a2ecddd96c739bbcd594dcc4a7b96ec1d002cc1e
EmoSola1403 <iamemoforeverfr@gmail.com>
2026-04-17 22:35:22 -0300

    Atualização '

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(" Calculadora de Soma Simples, Clique para continuar... ");
+Console.ReadKey();
+
+Console.Write("Digite o Primeiro número ");
+// lê a entrade e converte para o numero decimal (double)
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo número ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double resultado = num1 + num2;
+
+Console.WriteLine("=============================================");
+Console.WriteLine($"Resultado: {num1} + {num2} = {resultado}");
+
